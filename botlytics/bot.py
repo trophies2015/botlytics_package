168bd782e5bcf1dc96b5e8be7d28b45d4fa471e6
trophies2015 <=>
2016-07-15 18:07:28 -0400

    added headers

--- botlytics/bot.py
+++ botlytics/bot.py
@@ -3,9 +3,9 @@ import requests
 def send_request(API_KEY, text, kind, conversation_identifier):
   URL = "http://www.botlytics.co/api/v1/messages?token=" + API_KEY
   if (len(conversation_identifier) > 0):
-      return requests.post(URL, {"message":{"text":text,"kind":kind,"conversation_identifier":conversation_identifier}})
+      return requests.post(URL, data={"message":{"text":text,"kind":kind,"conversation_identifier":conversation_identifier}}, headers={"content-type":"application/json"})
   else:
-      return requests.post(URL, {"message":{"text":text,"kind":kind}})
+      return requests.post(URL, data={"message":{"text":text,"kind":kind}}, headers={"content-type":"application/json"})
 
 ta = send_request("7d1621e65ebb3f29", "heloe", "incoming", "okok")
 print(ta.text)
